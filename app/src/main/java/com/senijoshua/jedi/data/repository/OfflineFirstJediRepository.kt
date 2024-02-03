d414de6cdfaf76e8ffd17f3f5269d9dbbb1765d8
Shayne3000 <alfred3000prime@hotmail.com>
2024-02-03 02:08:10 -0500

    cleaning up v.5

--- app/src/main/java/com/senijoshua/jedi/data/repository/OfflineFirstJediRepository.kt
+++ app/src/main/java/com/senijoshua/jedi/data/repository/OfflineFirstJediRepository.kt
@@ -8,7 +8,6 @@ import com.senijoshua.jedi.data.remote.JediApi
 import com.senijoshua.jedi.data.util.Result
 import com.senijoshua.jedi.data.util.asResult
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.distinctUntilChanged
 import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.onEach
 import java.util.concurrent.TimeUnit
@@ -21,9 +20,6 @@ import javax.inject.Inject
  *
  * This facilitates an offline-first paradigm by employing the DB as
  * the single source of truth for getting a list of Jedis.
- *
- * NB: Data source classes aren't explicitly used as that would just be unnecessary bloat at this
- * scale.
  */
 class OfflineFirstJediRepository @Inject constructor(
     private val apiService: JediApi,
@@ -45,7 +41,7 @@ class OfflineFirstJediRepository @Inject constructor(
                     val jediResponse = apiService.getJedis()
                     db.insertAll(jediResponse.results.toLocal())
                 }
-            }.distinctUntilChanged().asResult()
+            }.asResult()
     }
 
     override suspend fun getJediById(jediId: Int): Result<Jedi> {
@@ -57,12 +53,11 @@ class OfflineFirstJediRepository @Inject constructor(
     }
 
     /**
-     * An invalidation paradigm for cached data that facilitates the retrieval
-     * of fresh data from the remote service if the currently held data
-     * has been stored for over an hour. It also implicitly verifies whether
-     * the table is empty or not.
-     *
-     * NB: `getTimeCreated()` would return null if the table is empty.
+     * An cache invalidation paradigm facilitates the retrieval
+     * of fresh data from the remote service if the currently held data is stale
+     * (i.e. has been stored for over an hour). It also implicitly verifies
+     * whether the table is empty or not as`getTimeCreated()` would return null
+     * if the table is empty.
      */
     private suspend fun jediTableIsEmptyOrHasStaleData(): Boolean {
         return (System.currentTimeMillis() - (db.getTimeCreated()
